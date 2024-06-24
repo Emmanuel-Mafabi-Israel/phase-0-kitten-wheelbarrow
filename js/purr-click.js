01e65f702b062612bfadd02f78b246d523fbc1d7
Mafabi-Israel-Emmanuel <israelmaleza01@outlook.com>
2024-06-24 20:10:43 +0300

    Created Kitten Wheelbarrow Project

---
+++ js/purr-click.js
@@ -0,0 +1,12 @@
+/*
+    GLORY BE TO GOD,
+    Kitten Wheelbarrow,
+    By Israel Mafabi Emmanuel
+*/
+
+$(function() {
+    let player1 = document.getElementById('player1');
+    $('img').click(function() {
+        player1.play();
+    });
+});
